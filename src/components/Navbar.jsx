2208f129953b99827687bb6102723f888a1b311e
Akhilesh <53186185+akhilesh-thakur@users.noreply.github.com>
2023-12-27 17:25:14 +0530

    Added my vercel website's link to Portfolio on Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -42,7 +42,7 @@ export default function Navbar() {
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
+              <a className="nav-link" href="https://akhilesh-react.vercel.app/">
                 Portfolio
               </a>
             </li>
